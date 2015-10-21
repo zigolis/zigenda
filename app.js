b30b802f5221891579010db7c92ca14cd71012aa
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2015-10-21 02:18:40 +0200

    Create app skeleton

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express')
+  , load = require('express-load')
+  , app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app)
+
+app.listen(app.get('port'), function() {
+  console.log('Running on port', app.get('port'));
+});
