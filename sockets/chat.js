6009df9d8a0f595dba44b3c17c4c12481761395f
Gabriel Zigolis <gabriel.zigolis@gmail.com>
2016-02-09 23:54:53 +0100

    Implement basic notification msg system

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,10 +1,18 @@
 module.exports = function(io) {
   var crypto = require('crypto')
-    , sockets = io.sockets;
+    , sockets = io.sockets
+    , onlines = {};
 
   sockets.on('connection', function(client) {
     var session = client.handshake.session
       , user = session.user;
+
+    onlines[user.email] = user.email;
+
+    for (var email in onlines) {
+      client.emit('notify-onlines', email);
+      client.broadcast.emit('notify-onlines', email);
+    }
 
     client.on('join', function(room) {
       if (!room) {
@@ -18,10 +26,6 @@ module.exports = function(io) {
       client.join(room);
     });
 
-    client.on('disconnect', function() {
-      client.leave(session.room);
-    });
-
     client.on('send-server', function(msg) {
       var room = session.room
         , data = {email: user.email, room: room};
@@ -30,6 +34,17 @@ module.exports = function(io) {
 
       client.broadcast.emit('new-message', data);
       sockets.in(room).emit('send-client', msg);
+    });
+
+    client.on('disconnect', function() {
+      var room = session.room
+        , msg = '<p><b>' + user.nome + ': </b> saiu.</p>';
+
+      client.broadcast.emit('notify-offlines', user.email);
+      client.leave(session.room);
+
+      sockets.in(room).emit('send-client', msg);
+      delete onlines[user.email];
     });
   });
 }
